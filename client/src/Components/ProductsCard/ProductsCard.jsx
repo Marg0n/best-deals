2aa86cdcdd17cd68bd2ef207e4d9a8fd662964b5
Shatak-the-imposter <helloshatak@gmail.com>
2024-09-20 01:45:57 +0600

    card hover moved to a component from homepage

--- client/src/Components/ProductsCard/ProductsCard.jsx
+++ client/src/Components/ProductsCard/ProductsCard.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Rating } from "@mui/material";
+import HoverdCardDetails from "../HoverdCardDetails/HoverdCardDetails";
 const ProductsCard = ({ product }) => {
   const {
     productName,
@@ -42,33 +42,13 @@ const ProductsCard = ({ product }) => {
           </h2>
         </div>
 
-
         {/* Hover effect */}
         <div>
-          {isHovered && (
-            <div
-              data-aos="fade-in"
-              data-aos-duration="1500"
-              className="absolute hero-overlay bg-opacity-90 z-50 right-full top-10 -mr-20 p-3 rounded-md shadow-md"
-              style={{ maxHeight: '15rem', maxWidth: '15rem' }}
-            >
-              <div className="mt-4 mb-2 bg-transparent">
-                <span className="block text-xs font-medium tracking-widest uppercase text-white ">
-                  Ratings :{ratings} <Rating name="half-rating"  size="small" defaultValue={ratings} precision={0.1}  />
-
-                </span>
-                <h2 className="text-xl font-semibold tracking-wide text-white">
-                  Brand : {brandName}
-                </h2>
-              </div>
-              <p className="text-white">
-                {description.length > 20 ? description.substring(0, 50) + '...' : description}
-              </p>
-              <Link to={`/details/${_id}`}>
-              <button className="btn bg-[#775050] border-none text-white w-full mt-3"> View Details</button>
-              </Link>
-            </div>
-          )}
+          {isHovered && 
+          <HoverdCardDetails
+          key={product._id}
+          product={product}
+          ></HoverdCardDetails>}
         </div>
       </div>
       </Link>
