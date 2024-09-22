02830fcea403dc953acb4756b20964ff8dfc1461
Shatak-the-imposter <helloshatak@gmail.com>
2024-09-22 22:20:40 +0600

    cart list page resposive and increment and decrement button working

--- client/src/Components/CartCard/CartCard.jsx
+++ client/src/Components/CartCard/CartCard.jsx
@@ -1,6 +1,22 @@
 import { Rating } from "@mui/material";
+import { useState } from "react";
+import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
 
 const CartCard = () => {
+    
+    const [count, setCount] = useState(0)
+
+    const handleIncrement = () => {
+        setCount(count + 1)
+    }
+    const handleDecrement = () => {
+        if (count === 0) {
+            setCount(0)
+        }
+        else {
+            setCount(count - 1)
+        }
+    }
     return (
         <div>
             <div className='flex items-center gap-2 bg-[#D9D9D9] p-3 rounded-[22px] mb-3'>
@@ -12,7 +28,19 @@ const CartCard = () => {
 
                             <Rating name="half-rating" size="small" defaultValue={'ratings'} precision={0.1} />
                         </span>
-                        <h5 className='text-[#1d2236] text-xl font-bold'>${'price'}</h5>
+                        <div className="flex justify-between gap-5 bg-[#775050] rounded-[3rem] p-1">
+                            <div>
+                                <button onClick={handleIncrement} className="btn btn-xs btn-circle mt-1 ">
+                                    <FiPlusCircle className="w-full h-full text-[#775050]" />
+                                </button>
+                            </div>
+                            <div className="text-white font-semibold text-lg">{count}</div>
+                            <div>
+                                <button onClick={handleDecrement} className="btn btn-xs btn-circle mt-1">
+                                    <FiMinusCircle className="w-full h-full text-[#775050]" />
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
