e6da0b643fe0b68aaf7ff6c7b62a34dcadedaf24
Shatak-the-imposter <helloshatak@gmail.com>
2024-09-25 01:55:47 +0600

    search functionality is working perfectly

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -4,41 +4,46 @@ import ProductsCard from "../../Components/ProductsCard/ProductsCard";
 import { ClimbingBoxLoader } from "react-spinners";
 import FeaturedProducts from "../../Components/FeaturedProducts/FeaturedProducts";
 import LeftMenubar from "../../Components/LeftMenuBar/LeftMenuBar";
+import useAxiosCommon from "../../hooks/useAxiosCommon";
+import { useState } from "react";
 
 const Home = () => {
-  const { data: products, isLoading } = useQuery({
-    queryKey: ["products"],
-    queryFn: async () => {
-      const res = await axios.get(`https://needscart-server.vercel.app/all`);
+  const axiosCommon = useAxiosCommon();
 
+  // search state data 
+  const [search, setSearch] = useState(""); 
+
+  const { data: products, isLoading } = useQuery({
+    queryKey: ["products", search], 
+    queryFn: async () => {
+      const res = await axiosCommon.get(`/all-products`, {
+        params:  { search } 
+      });
       return res.data;
     },
   });
+
   return (
-    <div className=" flex p-5 gap-5">
-      {/*Left Side menubar / categorybar  */}
-      <div  className="flex-1">
-        <LeftMenubar></LeftMenubar>
+    <div className="flex p-5 gap-5">
+      {/* Left Side menubar / category bar */}
+      <div className="flex-1">
+        <LeftMenubar 
+        setSearch={setSearch} />
       </div>
 
-      
       <div className="w-full lg:w-3/4 ">
-      <FeaturedProducts></FeaturedProducts>
-        {isLoading ?
-          (
-            <div className="flex justify-center items-center h-screen">
-              <ClimbingBoxLoader color="#36d7b7" />
-            </div>
-          ) :
-          (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
-              {products?.map(product =>
-              <ProductsCard
-              key={product._id}
-              product={product}
-              ></ProductsCard>)}
-            </div>      
-          )}
+        <FeaturedProducts />
+        {isLoading ? (
+          <div className="flex justify-center items-center h-screen">
+            <ClimbingBoxLoader color="#36d7b7" />
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
+            {products?.map((product) => (
+              <ProductsCard key={product._id} product={product} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
