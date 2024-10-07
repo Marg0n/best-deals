ab4cf0dea6538fa935bc734c6485f551723ff047
MarGoN <monondcosta@gmail.com>
2024-10-08 01:03:01 +0600

    Update: Stripe billing address now gets transId, statCard chages, user dashboard changes

--- client/src/Components/Dashboards/UserDashboard/UserHome/UserHome.jsx
+++ client/src/Components/Dashboards/UserDashboard/UserHome/UserHome.jsx
@@ -1,28 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import StatCard from '../../Shared/StatCard';
 import ProfileInfo from '../../Shared/ProfileInfo';
 import useUserProfile from '../../../../hooks/useUserProfile';
+import PropTypes from 'prop-types';
 
 const UserHome = () => {
 
+    // profile info
     const { profile } = useUserProfile();
-    console.log('billing',profile[0]?.billingAddress)
-    console.log('buy',profile[0]?.purchaseHistory)
+    console.log('billing', profile[0]?.billingAddress)
+    console.log('buy', profile[0]?.purchaseHistory)
+
+    // total spending stat
+    const [totalSpending, setTotalSpending] = useState();
+
+
+    // looping through all purchases amount
+    useEffect(() => {
+        if (profile && profile[0]?.purchaseHistory) {
+            const total = profile[0]?.purchaseHistory
+                ?.map(purchase => purchase?.totalAmount)
+                .reduce((sum, amount) => sum + amount, 0);
+            setTotalSpending(total);
+        }
+    }, [profile]);
+
 
     return (
-        <div className="p-8 bg-gray-50 min-h-screen space-y-4">
-        {/* stat section */}
-        <StatCard/>
+        <div className="p-8 min-h-screen space-y-4">
+            {/* stat section */}
+            <div className='flex gap-2 shadow-lg rounded-md'>
+                <StatCard
+                    title={'Total Times Buy'}
+                    value={profile[0]?.purchaseHistory.length}
+                    // percentage={25}
+                    // increase={true}
+                    // description={'more'}
+                />
+                <StatCard
+                    title={'Total Spendings'}
+                    value={totalSpending}
+                    // percentage={25}
+                    // increase={true}
+                    // description={'more'}
+                />
+            </div>
 
-        {/* profile info */}
-        <ProfileInfo/>
+            {/* profile info */}
+            <ProfileInfo />
 
-        {/* Account settings */}
+            {/* Account settings */}
 
-        {/* Chart section */}
-            
+            {/* Chart section */}
+
         </div>
     );
+};
+
+UserHome.propTypes = {
+    title: PropTypes.string,
+    value: PropTypes.string,
+    percentage: PropTypes.number,
+    increase: PropTypes.bool,
+    description: PropTypes.string,
 };
 
 export default UserHome;
