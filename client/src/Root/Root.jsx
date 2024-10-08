f8e6223ed4b58b50a4d9e768e0c92c4d832e39b3
Shatak-the-imposter <helloshatak@gmail.com>
2024-10-08 21:37:05 +0600

    working on cart list

--- client/src/Root/Root.jsx
+++ client/src/Root/Root.jsx
@@ -5,20 +5,9 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Footer from "../Components/Footer/Footer";
 import { Toaster } from "react-hot-toast";
-import useAuth from "../hooks/useAuth";
-import { loadState, saveState } from "../utils/localStorage";
-import { useDispatch } from "react-redux";
-import store from "../app/store";
-import { addToCart} from "../features/CartSlice/CartSlice";
+
 
 const Root = () => {
-  // get user id from firebase
-  const { user } = useAuth()
-  const userID = user?.uid
-  console.log(userID);
-
-
-  const dispatch = useDispatch()
 
   // Dark mode light mode control
   const [theme, setTheme] = useState(() => {
@@ -57,27 +46,7 @@ const Root = () => {
     });
   }, []);
 
-  // user data 
-  useEffect(() => {
-    if (userID) {
-      // Load the cart state from localStorage for the specific user
-      const persistedState = loadState(userID);
-      if (persistedState) {
-        // Dispatch the loaded cart state to Redux
-        dispatch(addToCart(persistedState.cart.cartIteams));
-      }
 
-      // Save the state to localStorage whenever the store changes
-      const unsubscribe = store.subscribe(() => {
-        saveState({
-          cart: store.getState().cart,
-        }, userID);
-      });
-
-      // Clean up the subscription when the component unmounts
-      return () => unsubscribe();
-    }
-  }, [userID, dispatch]);
 
   return (
     <div className="bg-gray-200 dark:bg-[#2F4161]">
--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,33 +1,33 @@
-// Function to save state to localStorage
-export const saveState = (state, userId) => { 
-    console.log(userId);
+// // Function to save state to localStorage
+// export const saveState = (state, userId) => { 
+//     console.log(userId);
     
-    try {
-        // if there is no user then return, nothing to save in local storage
-        if (!userId) return;
+//     try {
+//         // if there is no user then return, nothing to save in local storage
+//         if (!userId) return;
 
-        const serializedState = JSON.stringify(state);
-        // Save the state under 'cartState' with userId
-        localStorage.setItem(`cartState-${userId}`, serializedState);
-    } catch (e) {
-        console.warn('Could not save state', e);
-    }
-};
+//         const serializedState = JSON.stringify(state);
+//         // Save the state under 'cartState' with userId
+//         localStorage.setItem(`cartState-${userId}`, serializedState);
+//     } catch (e) {
+//         console.warn('Could not save state', e);
+//     }
+// };
 
-// Function to load state from localStorage
-export const loadState = (userId) => {
-    try {
-        // If there is no user, return undefined
-        if (!userId) return undefined;
+// // Function to load state from localStorage
+// export const loadState = (userId) => {
+//     try {
+//         // If there is no user, return undefined
+//         if (!userId) return undefined;
 
-        const serializedState = localStorage.getItem(`cartState-${userId}`);
-        if (serializedState === null) {
-            // If no state in localStorage, return undefined
-            return undefined;
-        }
-        return JSON.parse(serializedState);
-    } catch (e) {
-        console.warn('Could not load state', e);
-        return undefined;
-    }
-};
+//         const serializedState = localStorage.getItem(`cartState-${userId}`);
+//         if (serializedState === null) {
+//             // If no state in localStorage, return undefined
+//             return undefined;
+//         }
+//         return JSON.parse(serializedState);
+//     } catch (e) {
+//         console.warn('Could not load state', e);
+//         return undefined;
+//     }
+// };
