441da55fcd2c0d6f64f2867007bfe3dd401e7e7b
MarGoN <monondcosta@gmail.com>
2024-10-16 10:42:12 +0600

    Fix: cart's wishlist UI

--- client/src/Components/CartCard/CartCard.jsx
+++ client/src/Components/CartCard/CartCard.jsx
@@ -32,30 +32,37 @@ const CartCard = ({ product }) => {
                 {/* prodcut image , name and price box */}
                 <div className="flex  flex-col md:flex-row justify-between items-center gap-4 w-full">
                     {/* image */}
-                    <img className='w-24  h-20 rounded-[27px]' src={productImage} alt="image" />
+                    <img className='md:w-24 w-56 md:h-20 h-40 rounded-[27px]' src={productImage} alt="image" />
 
-                    <div className='flex items-center justify-between w-full'>
-                        <h1 className='text-[#020202] ml-2 text-base font-normal pb-5'>{product?.veriation}</h1>
-                        <span className="text-xs font-medium tracking-widest uppercase text-[#1d2236] flex items-center gap-2">
-                            ${price}
-                        </span>
+                    {/* details */}
+                    <div className=" flex flex-col md:flex-row flex-1 gap-4 w-full">
+                        <p className='text-[#020202] text-base text-center md:text-left font-bold md:w-1/2 '>{productName}</p>
+
+                        <div className='flex flex-col text-center md:text-left md:w-1/2 '>
+                            <h1 className='text-[#020202] text-base font-normal'>{product?.veriation}</h1>
+                            <span className="text-xs font-medium tracking-widest uppercase text-[#1d2236] gap-2">
+                                ${price}
+                            </span>
+                        </div>
                     </div>
+
+                </div>
+
+                {/* quantity and delete button box */}
+                <div className="flex justify-between items-center w-2/3 md:w-auto">
+                    <div className="flex flex-col lg:flex-row justify-between gap-5 bg-[#775050] rounded-[3rem] p-1 w-full md:w-auto">
+                        <div>
+                            <ProductsCounter
+                                key={product._id}
+                                product={product}
+                            ></ProductsCounter>
+                        </div>
+                    </div>
+                    {/* remove from cart button */}
+                    <button><TiDeleteOutline onClick={() => handleDelete(product._id)} className="w-8 h-8 text-[#775050]" /></button>
                 </div>
             </div>
 
-            {/* quantity and delete button box */}
-            <div className="flex justify-between items-center">
-                <div className="flex flex-col lg:flex-row justify-between gap-5 bg-[#775050] rounded-[3rem] p-1">
-                    <div>
-                        <ProductsCounter
-                            key={product._id}
-                            product={product}
-                        ></ProductsCounter>
-                    </div>
-                </div>
-                {/* remove from cart button */}
-                <button><TiDeleteOutline onClick={() => handleDelete(product._id)} className="w-8 h-8 text-[#775050]" /></button>
-            </div>
         </div>
 
     );
