7036954b6feebd3763c26009e3026cb288d6658a
Noc1 PC MargoN <margon.dcosta@cccul.com>
2024-10-27 15:48:55 +0600

    Update: minor visual changes in vendor request section in admin pannel

--- client/src/Components/Dashboards/AdminDashboard/AdminVendorRequest/AdminVendorRequest.jsx
+++ client/src/Components/Dashboards/AdminDashboard/AdminVendorRequest/AdminVendorRequest.jsx
@@ -49,7 +49,7 @@ const AdminVendorRequest = () => {
 
     const [searchTerm, setSearchTerm] = useState("");
     const [currentPage, setCurrentPage] = useState(0);
-    const rowsPerPage = 5; // Set number of rows per page
+    const rowsPerPage = 4; // Set number of rows per page
 
     const filteredData = initialData.filter((vendor) =>
         vendor.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -89,7 +89,7 @@ const AdminVendorRequest = () => {
                 const vendStatus = {
                     status: 'Approved',
                     reason: '',
-                    role:'Vendor'
+                    role: 'Vendor'
                 }
                 const role = 'Vendor'
                 const res = await vendorRequests.patch(`/allUser/${mail}`, vendStatus, role)
@@ -150,91 +150,94 @@ const AdminVendorRequest = () => {
     }
 
     return (
-        <div className="p-4 bg-white rounded-lg">
-            <h1 className="text-3xl mb-4 text-black">All Vendor Requests</h1>
+        <div className="p-6 min-h-screen space-y-4">
+            <div className="p-2 bg-white rounded-lg">
+                <h1 className="text-3xl mb-4 text-black">All Vendor Requests</h1>
 
-            <div className="w-1/3 mb-4">
-                <TextField
-                    label="Search by Vendor Name"
-                    variant="outlined"
-                    className="bg-gray-200"
-                    fullWidth
-                    margin="normal"
-                    onChange={(e) => {
-                        setSearchTerm(e.target.value);
-                        setCurrentPage(0); // Reset to first page on search
-                    }}
-                />
-            </div>
+                <div className="w-1/3 mb-4">
+                    <TextField
+                        label="Search by Vendor Name"
+                        variant="outlined"
+                        className="bg-gray-200"
+                        fullWidth
+                        margin="normal"
+                        onChange={(e) => {
+                            setSearchTerm(e.target.value);
+                            setCurrentPage(0); // Reset to first page on search
+                        }}
+                    />
+                </div>
 
-            <div className="border-2">
-                <TableContainer component={Paper}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell>Email</TableCell>
-                                <TableCell>Company</TableCell>
-                                <TableCell>Phone Number</TableCell>
-                                <TableCell>Document</TableCell>
-                                <TableCell>Status</TableCell>
-                                <TableCell>Action</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {currentRows.map((row, rowIndex) => (
-                                <TableRow key={rowIndex}>
-                                    <TableCell>{row.name}</TableCell>
-                                    <TableCell>{row.email}</TableCell>
-                                    <TableCell>{row.companyName}</TableCell>
-                                    <TableCell>{row.phoneNumber}</TableCell>
-                                    <TableCell>
-                                        <a className="btn hover:bg-gray-300 hover:text-black" target="_blank" href={row.pdfUrl}>Show Document</a>
-                                    </TableCell>
-                                    <TableCell>{row.vendorStatus.status}</TableCell>
-                                    <TableCell>
-                                        <div className="text-center">
-                                        {
-                                        (row?.vendorStatus?.status ==='Approved' || "Declined")
-                                        ? (
-                                            row?.vendorStatus?.status ==='Approved'
-                                            ? <button onClick={() => handleDecline(row.email)} className="bg-red-600 text-white p-2 rounded-lg hover:bg-gray-700">Revert to User</button>
-                                            : <button onClick={() => handleApprove(row.email)} className="bg-green-600 text-white p-2 rounded-lg mr-2 hover:bg-gray-700">Approve</button>
-                                        ) 
-                                        :<>
-                                            <button onClick={() => handleApprove(row.email)} className="bg-green-600 text-white p-2 rounded-lg mr-2 hover:bg-gray-700">Approve</button>
-                                            <button onClick={() => handleDecline(row.email)} className="bg-red-600 text-white p-2 rounded-lg hover:bg-gray-700">Decline</button>
-                                            </>
-                                        }
-                                        </div>
-                                    </TableCell>
+                <div className="border-2">
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>Email</TableCell>
+                                    <TableCell>Company</TableCell>
+                                    <TableCell>Phone Number</TableCell>
+                                    <TableCell>Document</TableCell>
+                                    <TableCell>Status</TableCell>
+                                    <TableCell>Action</TableCell>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </div>
+                            </TableHead>
+                            <TableBody>
+                                {currentRows.map((row, rowIndex) => (
+                                    <TableRow key={rowIndex}>
+                                        <TableCell>{row.name}</TableCell>
+                                        <TableCell>{row.email}</TableCell>
+                                        <TableCell>{row.companyName}</TableCell>
+                                        <TableCell>{row.phoneNumber}</TableCell>
+                                        <TableCell>
+                                            <a className="btn hover:bg-gray-300 hover:text-black" target="_blank" href={row.pdfUrl}>Show Document</a>
+                                        </TableCell>
+                                        <TableCell>{row.vendorStatus.status}</TableCell>
+                                        <TableCell>
+                                            <div className="text-center">
+                                                {
+                                                    (row?.vendorStatus?.status === 'Approved' || "Declined")
+                                                        ? (
+                                                            row?.vendorStatus?.status === 'Approved'
+                                                                ? <button onClick={() => handleDecline(row.email)} className="bg-red-600 text-white p-2 rounded-lg hover:bg-gray-700">Revert to User</button>
+                                                                : <button onClick={() => handleApprove(row.email)} className="bg-green-600 text-white p-2 rounded-lg mr-2 hover:bg-gray-700">Approve</button>
+                                                        )
+                                                        : <>
+                                                            <button onClick={() => handleApprove(row.email)} className="bg-green-600 text-white p-2 rounded-lg mr-2 hover:bg-gray-700">Approve</button>
+                                                            <button onClick={() => handleDecline(row.email)} className="bg-red-600 text-white p-2 rounded-lg hover:bg-gray-700">Decline</button>
+                                                        </>
+                                                }
+                                            </div>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
 
-            <div className="flex justify-between items-center my-4">
-                <Button
-                    onClick={handlePreviousPage}
-                    disabled={currentPage === 0}
-                    variant="outlined"
-                >
-                    Previous
-                </Button>
-                <Typography>
-                    Page {currentPage + 1} of {totalPages}
-                </Typography>
-                <Button
-                    onClick={handleNextPage}
-                    disabled={currentPage === totalPages - 1}
-                    variant="outlined"
-                >
-                    Next
-                </Button>
+                <div className="flex justify-between items-center my-4">
+                    <Button
+                        onClick={handlePreviousPage}
+                        disabled={currentPage === 0}
+                        variant="outlined"
+                    >
+                        Previous
+                    </Button>
+                    <Typography>
+                        Page {currentPage + 1} of {totalPages}
+                    </Typography>
+                    <Button
+                        onClick={handleNextPage}
+                        disabled={currentPage === totalPages - 1}
+                        variant="outlined"
+                    >
+                        Next
+                    </Button>
+                </div>
             </div>
         </div>
+
     );
 };
 
